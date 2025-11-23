34d14ae7db674fa110dd0e90a099e5ec0d7b3870
liliamezghani <l.mezghani.lm@gmail.com>
2025-11-23 23:00:10 +0100

    TP6 RMI - Service de conversion devises

---
+++ src/rmiServer/ConversionServer.java
@@ -0,0 +1,36 @@
+package rmiServer;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import rmiService.ConversionImpl;
+import rmiService.IConversion;
+
+public class ConversionServer {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Demarrage du serveur RMI");
+            
+            Registry registry = LocateRegistry.createRegistry(1099);
+            System.out.println("Registry RMI cree sur le port 1099");
+            
+            IConversion conversionService = new ConversionImpl();
+            System.out.println("Service ConversionImpl cree");
+            
+            registry.rebind("ConversionService", conversionService);
+            System.out.println("Service enregistre sous le nom ConversionService");
+            
+            System.out.println("Serveur RMI pret et en attente de connexions");
+            System.out.println("URL: rmi://localhost:1099/ConversionService");
+            System.out.println("Pour arreter le serveur, appuyez sur Ctrl+C");
+            
+            // Garder le serveur actif
+            while(true) {
+                Thread.sleep(1000);
+            }
+            
+        } catch (Exception e) {
+            System.err.println("Erreur serveur: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/rmiService/ConversionImpl.java
@@ -0,0 +1,16 @@
+package rmiService;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class ConversionImpl extends UnicastRemoteObject implements IConversion {
+    
+    public ConversionImpl() throws RemoteException {
+        super();
+    }
+    
+    @Override
+    public double convertirMontant(double mt) throws RemoteException {
+        return mt * 3.407;
+    }
+}
